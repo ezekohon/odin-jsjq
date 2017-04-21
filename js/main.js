45a2f9a81ded3bc312fd4c674f212dbd13681839
Eze Kohon <ezekohon@gmail.com>
2017-04-21 15:43:36 -0300

    All finished

--- js/main.js
+++ js/main.js
@@ -1,22 +1,49 @@
 $(document).ready(function() {
 
-    var $grid = $('.container');
 
-    for (i = 0; i < 16; i++) {
-        var row = '<div class="flex-container">';
+    $('#inButton').click(function() {
+        let num = $('#num').val();
+        clearGrid();
+        createGrid(num);
+        hover();
+    });
 
-        for (j = 0; j < 16; j++) {
-            row += '<div class="flex-item"></div>';
+function hover(){
+    $(".flex-item").hover(function() {
+        $(this).addClass('changeColor');
+        console.log('hover');
+    });
+}
+
+
+    function createGrid(num) {
+
+        var $grid = $('.container');
+
+        for (i = 0; i < num; i++) {
+            var row = '<div class="flex-container">';
+
+            for (j = 0; j < num; j++) {
+                row += '<div class="flex-item"></div>';
+            }
+
+            row += '</div>';
+
+
+            $grid.append(row);
+            // $(".square").css('width', 640/16);
+            // $(".square").css('height', 640/16);
         }
-
-        row += '</div>';
-
-
-        $grid.append(row);
-        // $(".square").css('width', 640/16);
-        // $(".square").css('height', 640/16);
     }
-    //try flexbox
+
+    function clearGrid() {
+        $('.container').empty();
+    }
+
+
+
+
+
 
 
 });
